715cbdf53a5cbc85a4171f42fe7c1ea39ee57402
Sathish-X Kumar <Sathish-X.Kumar@analog.com>
2025-03-27 18:05:28 +0530

    add Bootstrap and jQuery, update Angular component to use Material modules

--- projects/sms/src/app/app.component.ts
+++ projects/sms/src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, MatSlideToggleModule, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
