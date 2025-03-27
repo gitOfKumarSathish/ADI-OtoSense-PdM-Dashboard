4a7527d6e6918d8183e0c130863e8416725662f3
Sathish-X Kumar <Sathish-X.Kumar@analog.com>
2025-03-27 19:20:31 +0530

    add theme colors and update button component with tooltip support

--- projects/common/src/lib/components/button/button.component.ts
+++ projects/common/src/lib/components/button/button.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   selector: 'lib-button',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, MatTooltipModule],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
